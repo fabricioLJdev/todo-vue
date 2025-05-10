8675c4d21875cabfc4da23421d34fcea3c66ec24
fabricioLJdev <fabricioj516gb@outlook.com>
2025-05-10 18:37:13 -0300

    criando todo-list em um arquivo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
+//importado css do bootstrap dentro da pas node_modules
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 createApp(App).mount('#app')
